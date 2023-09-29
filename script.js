374b7fbb23259fa4ec6bfd1d5d39fe1e789c5e6b
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-09-29 16:06:30 +0530

    Modified the save function

--- script.js
+++ script.js
@@ -18,20 +18,11 @@ let saveFile = () => {
         'Message: ' + msg.value;
 
     // Convert the text to BLOB.
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'formData.txt';	   // The file to save the data.
+    const blobObject = new Blob([data], { type: 'text/plain' });
 
-    let newLink = document.createElement("a");
-    newLink.download = sFileName;
-
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
-
-    newLink.click();
+    let newLink = document.createElement("a");// Create an anchor tag.
+    newLink.download = 'formData.txt'; // The file to save the data.
+    newLink.href = URL.createObjectURL(blobObject);// Create url for file and add it to link's href.
+    newLink.click();// Click to download the form data file.
+    URL.revokeObjectURL(newLink.href);// Revoke the link.
 }
